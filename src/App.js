835d7eeaeaf2e4dcb899f8c9f0347e1a4f843856
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2020-03-02 15:45:55 -0600

    learned to render Food component conditionally, changed App component to class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Components/Person';
 import Food from './Components/Food'
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, welcome to my sandbox!</h1>
-      <h3>I'm excited to be learning :)</h3>
-      <Person name="Andrew" age="28"/>
-      <Person name="Amelia" age="25"/>
-      <Person></Person>
-      <Food foodName="PIZZA"/>
-    </div>
-  );
+
+
+
+class App extends Component {
+
+state = {
+  showFoods : false
+}
+
+ toggleFoodsHandler = () => {
+    const doesShow = this.state.showFoods;
+    this.setState({showFoods: !doesShow});
+ }
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello, welcome to my sandbox!</h1>
+        <h3> I'm excited to be learning :)</h3>
+        <Person name="Andrew" age="28"/>
+        <Person name="Amelia" age="25"/>
+        <button onClick={this.toggleFoodsHandler}>Show foods</button>
+        {this.state.showFoods ?
+          <div>
+            <Food/> 
+            </div> : null
+        } 
+      </div>
+    );
+  }
 }
 
 export default App;
